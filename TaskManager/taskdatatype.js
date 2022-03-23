b74a126226f7f440ad810611436a47725438cb44
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-03-23 02:15:07 -0500

    Delete TaskManager directory

--- TaskManager/taskdatatype.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Taskdatatype({ id, title, description }) {
-    return ( <
-        div > { ' ' } { id } { title } { description } { ' ' } <
-        /div>
-    );
-}

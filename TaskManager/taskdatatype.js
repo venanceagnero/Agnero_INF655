91242592896e0d391224cd06d5fa085d9b04f87d
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-03-23 02:08:24 -0500

    Add files via upload

---
+++ TaskManager/taskdatatype.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Taskdatatype({ id, title, description }) {
+    return ( <
+        div > { ' ' } { id } { title } { description } { ' ' } <
+        /div>
+    );
+}

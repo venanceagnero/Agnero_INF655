219fc24506c513c66dc63fbfa5018ac5c5b8fa26
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 10:06:01 -0500

    Update footer.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/pages/pagepartials/footer.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/pages/pagepartials/footer.js
@@ -1,1 +1,43 @@
 
+import React from 'react';
+import {
+    FaFacebook,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedin,
+    FaHouseUser,
+} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Footer() {
+    return ( <
+        div className = "footer" > { ' ' } <
+        div className = "links" > { ' ' } <
+        FaFacebook size = "30" / >
+        <
+        /div>{' '} <
+        div className = "links" > { ' ' } <
+        FaTwitter size = "30" / >
+        <
+        /div>{' '} <
+        div className = "links" > { ' ' } <
+        FaInstagram size = "30" / >
+        <
+        /div>{' '} <
+        div className = "links" >
+        <
+        FaLinkedin size = "30" / > { ' ' } <
+        /div>{' '} <
+        div className = "links" >
+        <
+        Link to = "/"
+        style = {
+            { color: 'white' } } >
+        <
+        FaHouseUser size = "30" / >
+        <
+        /Link>{' '} <
+        /div>{' '} <
+        /div>
+    );
+}

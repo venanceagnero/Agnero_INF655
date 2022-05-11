c7f69e21ae5b345a7ff1f62ce18a04c439f39472
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 10:15:21 -0500

    Update policyPage.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/pages/policyPage.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/pages/policyPage.js
@@ -1,1 +1,29 @@
 
+import React from 'react';
+import Header from './pagepartials/header';
+import Footer from './pagepartials/footer';
+import Card from '../Shared/Card';
+
+function PolicyPage() {
+    return ( <
+        >
+        <
+        div className = "pagebody" >
+        <
+        Header / >
+        <
+        h3 className = "Title" > Movie policy < /h3>{' '} <
+        Card >
+        <
+        p > { ' ' }
+        Our movie policy requires that all movie which must be added to the favorite list need be from our movie list.Any add which does not match to our movies will be removed form the app. { ' ' } <
+        /p>{' '} <
+        /Card>{' '} <
+        Footer / >
+        <
+        /div>{' '} <
+        />
+    );
+}
+
+export default PolicyPage;

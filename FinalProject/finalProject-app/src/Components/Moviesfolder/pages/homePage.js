bacb1df760df9365952758b24635f43fb1020bf5
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 10:12:13 -0500

    Update homePage.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/pages/homePage.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/pages/homePage.js
@@ -1,1 +1,40 @@
+//import './App.css';
+import React from 'react';
+import Header from './pagepartials/header';
+import Footer from './pagepartials/footer';
+import Navigation from './pagepartials/navigation';
+//import AddMovie from '../Moviesfolder/AddMovie';
+//import SearchMovie from '../Moviesfolder/searchMovie';
+import FavoriteMovies from '../Moviesfolder/FavoriteMovies';
+import Movieplay from '../Moviesfolder/Movieplay';
 
+export default function HomePage() {
+    //const [tasklist, setTasklist] = useState(taskData);
+    // const [addingtask, setAddingTask] = useState('');
+    return ( <
+        >
+        <
+        div className = "pagebody" > { ' ' } <
+        Header / >
+        <
+        div className = "row" >
+        <
+        div className = "col-sm-4 col-lg-2" > { ' ' } <
+        Navigation / >
+        <
+        /div>{' '} <
+        div className = "col-sm-4 col-lg-6" > { ' ' } <
+        Movieplay / >
+        <
+        /div>{' '} <
+        div className = "col-sm-4 col-lg-4" >
+        <
+        FavoriteMovies / >
+        <
+        /div>{' '} <
+        /div>{' '} <
+        Footer / > { ' ' } <
+        /div>{' '} <
+        />
+    );
+}

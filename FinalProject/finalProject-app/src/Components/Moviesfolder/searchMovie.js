b50071c83a27162b01b9fea4df1961151b9f9123
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 15:08:07 -0500

    Update searchMovie.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/searchMovie.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/searchMovie.js
@@ -1,1 +1,25 @@
 
+import React from 'react';
+import Card from '../Shared/Card';
+
+export default function SearchMovie({ search, setSearch }) {
+    return ( <
+        Card className = "card" > { ' ' } <
+        form className = "inputForm "
+        onSubmit = {
+            (elmt) => elmt.preventDefault() } > { ' ' } <
+        h5 className = "Title" > Search movie < /h5>{' '} <
+        div className = "inputField" > { ' ' } <
+        input className = "searchform "
+        id = "search"
+        type = "text"
+        role = "searchbox"
+        placeholder = "search task"
+        onChange = {
+            (elmt) => setSearch(elmt.target.value) }
+        />{' '} <
+        /div>{' '} <
+        /form>{' '} <
+        /Card>
+    );
+}

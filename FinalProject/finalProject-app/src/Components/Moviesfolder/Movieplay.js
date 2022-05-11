7f88f832c239d642b8324f9d0e040f4691b8afd6
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 15:04:33 -0500

    Update Movieplay.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/Movieplay.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/Movieplay.js
@@ -1,1 +1,15 @@
 
+import React from 'react';
+import movieimg from '../assets/sampleimg.png';
+
+export default function Movieplay() {
+    return ( <
+        div className = "movieVideoContainer" >
+        <
+        img src = { movieimg }
+        alt = "movie photo"
+        className = "MovieImg" / >
+        <
+        /div>
+    );
+}

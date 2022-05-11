d32b3ac877523bac481f851093faf6e7822f96b9
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 10:10:39 -0500

    Update createMovieListPage.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/pages/createMovieListPage.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/pages/createMovieListPage.js
@@ -1,1 +1,19 @@
 
+import React from 'react';
+import Footer from './pagepartials/footer';
+import Header from './pagepartials/header';
+//import Userlogin from '../User/Userlogin';
+import AddMovie from '../Moviesfolder/AddMovie';
+
+export default function CreateMovieListpage() {
+    return ( <
+        > { ' ' } <
+        div className = "pagebody" > { ' ' } <
+        Header / > < AddMovie / >
+        <
+        Footer / >
+        <
+        /div>{' '} <
+        />
+    );
+}

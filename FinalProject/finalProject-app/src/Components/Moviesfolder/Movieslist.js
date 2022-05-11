40c5205730334f5d98ec61f270b1a079730bd86a
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-11 15:06:41 -0500

    Update Movieslist.js

--- FinalProject/finalProject-app/src/Components/Moviesfolder/Movieslist.js
+++ FinalProject/finalProject-app/src/Components/Moviesfolder/Movieslist.js
@@ -1,1 +1,39 @@
 
+import { useState, useContext } from 'react';
+import MovieContext from '../context/moviecontext';
+import Spinner from '../Shared/spinner';
+import Movie from './Movie';
+
+export default function Movieslist() {
+    const { movielist, isLoading } = useContext(MovieContext);
+    const [search, setSearch] = useState('');
+
+    const result = movielist.filter((movie) =>
+        movie.movie_name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (!isLoading && (!result || result.length === 0)) {
+        return <p > No movie < /p>;
+    }
+
+    return isLoading ? ( <
+        Spinner / >
+    ) : ( <
+        div > { ' ' } {
+            result.map((movie) => ( <
+                Movie key = { movie.id }
+                id = { movie.id }
+                movie_name = { movie.movie_name }
+                director = { movie.director }
+                category = { movie.category }
+                release_year = { movie.release_year }
+                image = { movie.image }
+                youtube_link = { movie.youtube_link }
+                ratings = { movie.ratings }
+                movie = { movie }
+                />
+            ))
+        } { ' ' } <
+        /div>
+    );
+}

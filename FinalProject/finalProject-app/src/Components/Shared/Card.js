c5a246b5ed0cd2f73fc47cb628a206ac0e03b1c3
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-12 23:36:27 -0500

    Update Card.js

--- FinalProject/finalProject-app/src/Components/Shared/Card.js
+++ FinalProject/finalProject-app/src/Components/Shared/Card.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+export default function Card({ children }) {
+    return ( <
+        > { ' ' } <
+        div className = "card" > { children } < /div>;{' '} <
+        />
+    );
+}
 
+// Card.propTypes = {
+//     children: PropTypes.node.isRequired,
+// };

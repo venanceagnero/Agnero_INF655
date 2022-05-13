e8b96d80189c93b9f95f653a51547418000bbaca
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-12 23:43:51 -0500

    Update db.js

--- FinalProject/finalProject-app/src/db.js
+++ FinalProject/finalProject-app/src/db.js
@@ -1,1 +1,46 @@
-
+{
+  "movies": [
+    {
+      "id": "ab1",
+      "movie_name": "Escaping",
+      "director": "John Branko",
+      "category": "comedy",
+      "release_year": "2022",
+      "image": " ",
+      "youtube_link": " ",
+      "ratings": "3",
+      "checked": false
+    },
+    {
+      "id": "abc2",
+      "movie_name": "Forest man",
+      "director": "Claude Brown",
+      "category": "comedy",
+      "release_year": "2022",
+      "image": " ",
+      "youtube_link": " ",
+      "ratings": "4",
+      "checked": false
+    },
+    {
+      "movie_name": "Nocker",
+      "director": "Jule Elders",
+      "category": "Comedy",
+      "release_year": "2016",
+      "image": "C:\\fakepath\\Capture.PNG",
+      "youtube_link": "eyerueuduwisis",
+      "ratings": "2",
+      "id": "7Neu7kD"
+    },
+    {
+      "movie_name": "the creatures",
+      "director": "Benson Claude",
+      "category": "Action",
+      "release_year": "2017",
+      "image": "C:\\fakepath\\Capture.PNG",
+      "youtube_link": "ryeudkofklc;v;f';v;v",
+      "ratings": "3",
+      "id": "28pJ6tR"
+    }
+  ]
+}

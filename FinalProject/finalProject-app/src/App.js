9c1d84dd4f5386200503a3c6678718bfcf1d7a26
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-12 23:39:42 -0500

    Update App.js

--- FinalProject/finalProject-app/src/App.js
+++ FinalProject/finalProject-app/src/App.js
@@ -1,1 +1,40 @@
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MovieProvider } from './Components/context/moviecontext';
+import PolicyPage from './Components/pages/policyPage';
+import HomePage from './Components/pages/homePage';
+//import Users from './Components/User/users';
+import MovieListpage from './Components/pages/movieListPage';
+import CreateMovieListpage from './Components/pages/createMovieListPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return ( <
+        >
+        <
+        MovieProvider >
+        <
+        BrowserRouter >
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < HomePage / > }
+        />{' '} <
+        Route path = "/policy"
+        element = { < PolicyPage / > }
+        />{' '} <
+        Route path = "/createmovielist"
+        element = { < CreateMovieListpage / > }
+        />{' '} <
+        Route path = "/movielist"
+        element = { < MovieListpage / > }
+        />{' '} <
+        /Routes>{' '} <
+        /BrowserRouter>{' '} <
+        /MovieProvider>{' '} <
+        />
+    );
+}
+
+export default App;

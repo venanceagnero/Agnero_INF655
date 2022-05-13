83504fec027eb70e6a3ceee728a3f608c1e5e5d6
venanceagnero <88169204+venanceagnero@users.noreply.github.com>
2022-05-12 23:37:27 -0500

    Update spinner.js

--- FinalProject/finalProject-app/src/Components/Shared/spinner.js
+++ FinalProject/finalProject-app/src/Components/Shared/spinner.js
@@ -1,1 +1,15 @@
 
+import * as React from 'react';
+import spinner from '../assets/ZKZg.gif';
+export default function Spinner() {
+    return ( <
+        div >
+        <
+        img src = { spinner }
+        alt = "Loading..."
+        style = {
+            { width: '100px', margin: 'auto', display: 'block' } }
+        />{' '} <
+        /div>
+    );
+}
